66f199c551c407cc04f76f1279eae8e752859ea6
Elias Simojoki <simo.simojoki@gmail.com>
2022-11-06 04:59:09 +0200

    Proper text

--- temp-front/src/Chats.js
+++ temp-front/src/Chats.js
@@ -50,7 +50,7 @@ function Chats() {
                   <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
                 </ListItemAvatar>
                 <ListItemText
-                  primary="Brunch this weekend?"
+                  primary="have u ever felt like u don't know who u r?"
                   secondary={(
                     <>
                       <Typography
@@ -61,7 +61,8 @@ function Chats() {
                       >
                         Ali Connors
                       </Typography>
-                      {" — I'll be in your neighborhood doing errands this…"}
+                      I do quite often feel that way. Somehow
+                      struggling to even understand myself...
                     </>
           )}
                 />
